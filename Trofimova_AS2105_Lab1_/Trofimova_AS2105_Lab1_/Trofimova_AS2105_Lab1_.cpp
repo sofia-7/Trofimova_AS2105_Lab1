e76d4aa7c348623623d5de5ff06b81db7e083ddd
sofia-7 <112961730+sofia-7@users.noreply.github.com>
2022-09-25 15:12:40 +0300

    Меню(2).Ввод\вывод файл.

--- Trofimova_AS2105_Lab1_/Trofimova_AS2105_Lab1_/Trofimova_AS2105_Lab1_.cpp
+++ Trofimova_AS2105_Lab1_/Trofimova_AS2105_Lab1_/Trofimova_AS2105_Lab1_.cpp
@@ -1,20 +1,19 @@
 ﻿
 #include <iostream>
 #include <string>
-#include <conio.h>
-
+#include <fstream>
 using namespace std;
 
 struct Tube
 {
 	float length=0, diametr=0;
-	bool maintenance;
+	int maintenance=-1;
 };
 
 struct KS
 {
 	string name="";
-	int count=0, workCount=0;
+	int workshops=0, workingWork=0;
 	float efficiency=0;
 };
 
@@ -25,37 +24,90 @@ int main()
 	Tube tb;
 	KS ks;
 	int menu = 0;
-	cout << "1.Add new tube.\n" << "2.Add new KS.\n" << "3.Added objects.\n" << "4.Change tube.\n" << "5.Change KS.\n" << "6.Add to file.\n" << "7.Exit\n" << ">";
+	cout << "1.Add new tube.\n" << "2.Add new KS.\n" << "3.Added objects.\n" 
+		<< "4.Change tube.\n" << "5.Change KS.\n" << "6.Add to file\n"
+		<<"7.Load from file.\n" << "0.Exit\n" << ">";
 	cin >> menu;
 	switch(menu)
 	{
-	case 1:
-	{
-		cout << "\nLength:";
-		cin >> tb.length;
-		cout << "\nDiametr:";
-		cin >> tb.diametr;
-		cout << "\nEnter 0 if tube is in repair and 1 if it works:";
-		cin >> tb.maintenance;
-		break;
+		case 1:
+		{
+			cout << "\nLength:";
+			cin >> tb.length;
+			cout << "\nDiametr:";
+			cin >> tb.diametr;
+			cout << "\nEnter 0 if tube is in repair and 1 if it works:";
+			cin >> tb.maintenance;
+			break;
+		}
+		case 2:
+		{
+			cout << "\nName:";
+			cin >> ks.name;
+			cout << "\nTotal number of workshops:";
+			cin >> ks.workshops;
+			cout << "\nNumber of working workshops:";
+			cin >> ks.workingWork;
+			break;
+		}
+		case 3:
+		{
+			cout << "\nTube.\n" << "\nLength:" << tb.length << "\nDiametr:" 
+				<< tb.diametr << "\nMaintanance:" << tb.maintenance << endl;
+			cout << "\nKS.\n" << "\nName:" << ks.name << "\nTotal number of workshops:" 
+				<< ks.workshops << "\nNumber of working workshops:" << ks.workingWork << endl;
+			break;
+		}
+		case 4:
+		{
+			cout << "\nInput new tube status (Enter 0 if tube is in repair and 1 if it works):";
+			cin >> tb.maintenance;
+			break;
+		}
+		case 5:
+		{
+			cout << "\nWorkshops:";
+			cin >> ks.workshops;
+			cout << "\nWorking workshops:";
+			cin >> ks.workingWork;
+			break;
+		}
+		case 6:
+		{
+			ofstream fout;
+			fout.open("Запись.txt");
+				fout << "\nTube" << endl << tb.length << endl << tb.diametr << endl << tb.maintenance << endl
+					<< "\nKS" << endl << ks.name << endl << ks.workshops << endl << ks.workingWork << endl
+					<< ks.efficiency << endl;
+				fout.close();
+				break;
+		}
+		case 7:
+		{
+			ifstream fin;
+			string line;
+			fin.open("Запись.txt");
+			getline(fin, line);
+			tb.length = stoi(line);
+			getline(fin, line);
+			tb.diametr = stoi(line);
+			getline(fin, line);
+			tb.maintenance = stoi(line);
+			getline(fin, line);
+			ks.name = line;
+			getline(fin, line);
+			ks.workshops = stoi(line);
+			getline(fin, line);
+			ks.workingWork = stoi(line);
+			getline(fin, line);
+			ks.efficiency = stoi(line);
+			break;
+		}
+		case 0:
+		{
+			return 0;
+		}
+
 	}
-	case 2:
-	{
-		cout << "\nName:";
-		cin >> ks.name;
-		cout << "\nTotal number of workshops:";
-		cin >> ks.count;
-		cout << "\nNumber of working workshops:";
-		cin >> ks.workCount;
-		break;
-	}
-	case 3:
-	{
-		cout << "\nTube.\n" << "\nLength:" << tb.length << "\nDiametr:" << tb.diametr << "\nMaintanance:" << tb.maintenance << endl;
-		cout << "\nKS.\n" << "\nName:" << ks.name << "\nTotal number of workshops:" << ks.count << "\nNumber of working workshops:" << ks.workCount << endl;
-		break;
-	}
-	}
-	_getch();
 	return 0;
 }
