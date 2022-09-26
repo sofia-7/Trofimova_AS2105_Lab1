f64730fc486921ee94038c14a5423885ab9317c4
sofia-7 <112961730+sofia-7@users.noreply.github.com>
2022-09-26 17:54:14 +0300

    Проверка на ошибки.

--- Trofimova_AS2105_Lab1_/Trofimova_AS2105_Lab1_/Trofimova_AS2105_Lab1_.cpp
+++ Trofimova_AS2105_Lab1_/Trofimova_AS2105_Lab1_/Trofimova_AS2105_Lab1_.cpp
@@ -7,7 +7,7 @@ using namespace std;
 struct Tube
 {
 	float length=0, diametr=0;
-	int maintenance=-1;
+	int maintenance=0;
 };
 
 struct KS
@@ -17,27 +17,96 @@ struct KS
 	float efficiency=0;
 };
 
+float numeric_check(float l)
+{
+	while ((l <= 0) || (!l))
+	{
+		cout << ("Enter number > 0.\n");
+		cin.clear();
+		cin.ignore(INT_MAX, '\n');
+		cin >> l;
+	}
+	return l;
+}
+
+int maint_check(int m)
+{
+	while ((!m) || (m < 1) || (m > 2))
+	{
+		cout << ("Enter 1 or 2.\n");
+		cin.clear();
+		cin.ignore(INT_MAX, '\n');
+		cin >> m;
+	}
+	return m;
+}
+
+string maint_check_2(int m)
+{
+	if (m == 1)
+		return ("Tube is in maintenance\n");
+	else
+		return ("Tube works\n");
+}
+
+int numeric_check_2(int w)
+{
+	while ((w <= 0) || (!w))
+	{
+		cout << ("Enter number > 0.\n");
+		cin.clear();
+		cin.ignore(INT_MAX, '\n');
+		cin >> w;
+	}
+	return w;
+}
+
+int working_worshops_check(int ww, int w)
+{
+	while ((ww <= 0) || (!ww) || (ww > w))
+	{
+		cout << ("Enter number > 0 and less than total amount of workshops.\n");
+		cin.clear();
+		cin.ignore(INT_MAX, '\n');
+		cin >> ww;
+	}
+	return ww;
+}
+void clear_file()
+{
+	ofstream out;
+	out.open("Запись.txt", ofstream::out | ofstream::trunc);
+	out.close();
+	cout<< "File is empty.\n";
+	return ;
+}
+
 
 int main()
 {
-
 	Tube tb;
 	KS ks;
-	int menu = 0;
-	cout << "1.Add new tube.\n" << "2.Add new KS.\n" << "3.Added objects.\n" 
-		<< "4.Change tube.\n" << "5.Change KS.\n" << "6.Add to file\n"
-		<<"7.Load from file.\n" << "0.Exit\n" << ">";
-	cin >> menu;
-	switch(menu)
+	int menu = -1;
+	while (menu)
 	{
+		cout << "1.Add new tube.\n" << "2.Add new KS.\n" << "3.Added objects.\n"
+			<< "4.Change tube.\n" << "5.Change KS.\n" << "6.Add to file\n"
+			<< "7.Load from file.\n" << "0.Exit\n" << ">";
+		cin >> menu;
+		switch (menu)
+		{
 		case 1:
 		{
 			cout << "\nLength:";
 			cin >> tb.length;
+			tb.length = numeric_check(tb.length);
 			cout << "\nDiametr:";
 			cin >> tb.diametr;
+			tb.diametr = numeric_check(tb.diametr);
 			cout << "\nEnter 0 if tube is in repair and 1 if it works:";
 			cin >> tb.maintenance;
+			tb.maintenance = maint_check(tb.maintenance);
+			cout << maint_check_2(tb.maintenance) << endl;
 			break;
 		}
 		case 2:
@@ -46,68 +115,128 @@ int main()
 			cin >> ks.name;
 			cout << "\nTotal number of workshops:";
 			cin >> ks.workshops;
+			ks.workshops = numeric_check_2(ks.workshops);
 			cout << "\nNumber of working workshops:";
 			cin >> ks.workingWork;
+			ks.workingWork = working_worshops_check(ks.workingWork, ks.workshops);
+			ks.efficiency = float(ks.workingWork) / float(ks.workshops)*100;
+			cout << ("\nEfficiency:") << ks.efficiency << "%"<< endl;
 			break;
 		}
 		case 3:
 		{
-			cout << "\nTube.\n" << "\nLength:" << tb.length << "\nDiametr:" 
-				<< tb.diametr << "\nMaintanance:" << tb.maintenance << endl;
-			cout << "\nKS.\n" << "\nName:" << ks.name << "\nTotal number of workshops:" 
-				<< ks.workshops << "\nNumber of working workshops:" << ks.workingWork << endl;
+			if(tb.length == 0)
+				cout << "There is no tube.\n";
+			else
+				cout << "\nTube.\n" << "\nLength:" << tb.length << "\nDiametr:"
+					<< tb.diametr << "\nMaintanance:" << maint_check_2(tb.maintenance) << endl;
+			if (ks.name == "")
+				cout << "There is no KS.\n";
+			else
+				cout << "\nKS.\n" << "\nName:" << ks.name << "\nTotal number of workshops:"
+					<< ks.workshops << "\nNumber of working workshops:" << ks.workingWork 
+					<<"\nEfficiency:" << ks.efficiency << endl;
 			break;
 		}
 		case 4:
-		{
-			cout << "\nInput new tube status (Enter 0 if tube is in repair and 1 if it works):";
-			cin >> tb.maintenance;
+		{	if (tb.maintenance == 0)
+			{
+			cout << "There is no tube to edit.\n";
+			break;
+			}
+			if (tb.maintenance !=0)
+				cout << "\nInput new tube status (Enter 1 if tube is in repair and 2 if it works):";
+				cin >> tb.maintenance;
+				tb.maintenance = maint_check(tb.maintenance);
+				cout << maint_check_2(tb.maintenance) << endl;
 			break;
 		}
 		case 5:
 		{
-			cout << "\nWorkshops:";
-			cin >> ks.workshops;
-			cout << "\nWorking workshops:";
-			cin >> ks.workingWork;
+			if (ks.workshops == 0)
+			{
+				cout << "There are no workshops to edit.\n";
+				break;
+			}
+			if (ks.workshops != 0)
+				cout << "\nWorkshops:";
+				cin >> ks.workshops;
+				ks.workshops = numeric_check_2(ks.workshops);
+				cout << "\nNumber of working workshops:";
+				cin >> ks.workingWork;
+				ks.workingWork = working_worshops_check(ks.workingWork, ks.workshops);
+				ks.efficiency = float(ks.workingWork) / float(ks.workshops) * 100;
+				cout << ("\nEfficiency:") << ks.efficiency << "%" << endl;
 			break;
 		}
 		case 6:
 		{
-			ofstream fout;
-			fout.open("Запись.txt");
-				fout << "\nTube" << endl << tb.length << endl << tb.diametr << endl << tb.maintenance << endl
-					<< "\nKS" << endl << ks.name << endl << ks.workshops << endl << ks.workingWork << endl
-					<< ks.efficiency << endl;
+			
+			if ((ks.name == "") & (tb.length == 0))
+			{
+				cout << "There is no data to load.\n";
+				break;
+			}
+			if ((ks.name != "") & (tb.length != 0))
+			{
+				cout << "Check file.\n";
+				ofstream fout;
+				fout.open("Запись.txt");
+				fout << "\nTube" << endl << "Length:" << tb.length << endl << "Diametr" << tb.diametr
+					<< endl << "Maintenance:" << tb.maintenance << endl
+					<< "\nKS" << endl << "Name:"<< ks.name << endl <<"Workshops:" <<ks.workshops 
+					<< endl <<"Working workshops:"<< ks.workingWork << endl<< "Efficiency: "<<ks.efficiency << endl;
+					fout.close();
+				break;
+			}
+			if ((tb.length == 0) &(ks.name != ""))
+			{
+				cout << "There is no data abot tube to load. Check file for ks information. \n";
+				ofstream fout;
+				fout.open("Запись.txt");
+				fout << "\nKS" << endl << "Name:" << ks.name << endl << "Workshops:" << ks.workshops
+					<< endl << "Working workshops:"<< ks.workingWork << endl << "Efficiency: "<<ks.efficiency << endl;
 				fout.close();
 				break;
+			}
+			if ((ks.name == "") & (tb.length != 0))
+			{
+				cout << "There is no data about KS to load. Check file for tube information.\n";
+				ofstream fout;
+				fout.open("Запись.txt");
+				fout << "\nTube" << endl << "Length:" << tb.length << endl << "Diametr" << tb.diametr
+					<< endl << "Maintenance:" << tb.maintenance << endl;
+				fout.close();
+				break;
+			}
+
+			break;
 		}
 		case 7:
 		{
-			ifstream fin;
+			if ((ks.name == "") & (tb.length == 0))
+				clear_file();
 			string line;
+			ifstream fin;
 			fin.open("Запись.txt");
-			getline(fin, line);
-			tb.length = stoi(line);
-			getline(fin, line);
-			tb.diametr = stoi(line);
-			getline(fin, line);
-			tb.maintenance = stoi(line);
-			getline(fin, line);
-			ks.name = line;
-			getline(fin, line);
-			ks.workshops = stoi(line);
-			getline(fin, line);
-			ks.workingWork = stoi(line);
-			getline(fin, line);
-			ks.efficiency = stoi(line);
+			if (fin.is_open())
+			{
+				while (getline(fin, line))
+				{
+					cout << line << endl;
+				}
+			}
+			fin.close();
 			break;
 		}
 		case 0:
 		{
 			return 0;
 		}
-
+		default:
+			cout << "Input number between 0 and 7" << endl;
+			break;
+		}
 	}
 	return 0;
 }
